88cc4cd844b25a7a5b4a79265baceef8d0f2169a
kamal <kamal.zyel@gmail.com>
2023-01-17 19:24:11 +0700

    install firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({	
-	runtimeConfig: {
-		public: {
-      nuxtAppName: process.env.NUXT_APP_NAME,
-    }
-    
-  }
+	modules: []
 })
